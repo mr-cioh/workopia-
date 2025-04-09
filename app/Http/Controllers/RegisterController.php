e26f54e44cc87c036bac2eae63540753115ba01d
Hoi <hkcheung1217@gmail.com>
2025-04-09 15:27:49 -0700

    finish RegisterController store method

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
@@ -17,8 +19,19 @@ class RegisterController extends Controller
 
     // @desc  Store new user
     // @route POST /register
-    public function store(): string
-    {
-        return 'store';
+    public function store(Request $request): RedirectResponse
+    {   
+        $validatedData = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|min:8|confirmed'
+        ]);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        $user = User::create($validatedData);
+        
+        return redirect()->route('home')->with('success', 'Registration successful You can now log in!');
+
     }
 }
